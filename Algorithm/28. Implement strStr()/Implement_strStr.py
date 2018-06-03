e70e3bcb909a989693ef389e75c38fdc52426d69
jhuang292 <jhuang292@wisc.edu>
2018-06-03 18:41:45 -0500

    Add files via upload

---
+++ Algorithm/28. Implement strStr()/Implement_strStr.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+                    
+                        
+                        
+                    
+            
